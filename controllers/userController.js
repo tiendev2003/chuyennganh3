974ece7e2bdc73775da244b1a50709dd7481f395
tiendev2003 <tientc.21it@vku.udn.vn>
2025-02-22 20:35:02 +0700

    update

--- controllers/userController.js
+++ controllers/userController.js
@@ -74,6 +74,7 @@ exports.seedAdmin = async (req, res) => {
     });
     res.status(201).json(user);
   } catch (error) {
+    console.log(error);
     res.status(400).json({ error: error.message });
   }
 };
